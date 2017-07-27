0d6b7c06c7f3e41bcd5f2452002b4926f15549a9
Manoj <manoj.rajendran@iopex.com>
2017-07-27 16:43:29 +0530

    changes

--- app.js
+++ app.js
@@ -20,6 +20,31 @@ app.controller('FormController', function($scope) {
   $scope.message = 'Hello from FormController';
 });
 
-app.controller('DetailsController', function($scope) {
+/*app.controller('DetailsController', function($scope) {
   $scope.message = 'Hello from DetailsController';
+});*/
+
+
+//var mainApp = angular.module("myApp", []);
+         
+ app.controller('DetailsController', function($scope) {
+    $scope.student = {
+       firstName: "Mahesh",
+       lastName: "Parashar",
+       fees:500,
+       
+       subjects:[
+          {name:'Physics',marks:70},
+          {name:'Chemistry',marks:80},
+          {name:'Math',marks:65},
+          {name:'English',marks:75},
+          {name:'Hindi',marks:67}
+       ],
+       
+       fullName: function() {
+          var studentObject;
+          studentObject = $scope.student;
+          return studentObject.firstName + " " + studentObject.lastName;
+       }
+    };
 });
